a284298096394da81e85df7ae103f4f92c4fbdff
Victor Saccucci <victorsaccucci@hotmail.com>
2023-07-27 10:23:02 -0300

    Exemples js

--- js/exemplos.js
+++ js/exemplos.js
@@ -1,5 +1,29 @@
-let prompt = require ('prompt-sync')();
-let curso = prompt('Qual o nome do seu curso');
-console.log('Curso: ' + curso);
-let false = Number(prompt('Informe a fase: '));
-console.log()
+let carroA = { fabrincante: "Ford", modelo: "Mustang", ano: 1969 }
+let carroB = { fabrincante: "porsche", modelo: "Macan", ano: 2023 }
+let carros = [carroA, carroB]
+
+for (let carro of carros) {
+    console.log(carro);
+}
+
+console.log("-------------------------")
+for (let propriedade in carroA) {
+    console.log(propriedade);
+}
+
+console.log("-------------------------")
+for (let propriedade in carroB) {
+    console.log("Propriedade: " + propriedade
+        + " - valor: " + carroB[propriedade]);
+}
+
+console.log("-------------------------")
+let nomes = ["Fred", "Sheila", "Sara"];
+for(let nome of nomes){
+    console.log(nomes);
+}
+
+console.log("-------------------------")
+for(let nome in nomes){
+    console.log(nome)
+}
