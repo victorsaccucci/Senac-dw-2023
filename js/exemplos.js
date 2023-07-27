426a902c9f90ac7287330015bfd46df120567d5a
Victor Saccucci <victorsaccucci@hotmail.com>
2023-07-27 09:31:30 -0300

    First commit

---
+++ js/exemplos.js
@@ -0,0 +1,5 @@
+let prompt = require ('prompt-sync')();
+let curso = prompt('Qual o nome do seu curso');
+console.log('Curso: ' + curso);
+let false = Number(prompt('Informe a fase: '));
+console.log()
